05972c1f7478dad7c25bc261fc9ba52b732f04c4
Binaya321 <Binayapaudel782@gmail.com>
2024-09-09 21:28:05 -0400

    Studio 2

---
+++ src/chapters/control-flow-and-collections/Exercises/collections-exercises.java
@@ -0,0 +1,9 @@
+public class MyMainClass {
+    public static void main(String[] args) {
+        int[] array = {1, 1, 2, 3, 5, 8};
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
